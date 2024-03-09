718b7100b6c07d6ca287f8c38b376cb1a9be3693
liiadavid <liiadavid@yandex.ru>
2024-03-09 22:47:18 +0300

    KS-2-1 посчитала средний балл

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main() {
+    val numberOfStudents = 4
+    val grade1 = 3.00
+    val grade2 = 4
+    val grade3 = 3
+    val grade4 = 5
+    val average = (grade1 + grade2 + grade3 + grade4) / numberOfStudents
+
+    println("Средний балл в классе - $average")
+}

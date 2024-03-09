2233ac825860ab38ab874d0e0df5a3b3862f7537
liiadavid <liiadavid@yandex.ru>
2024-03-09 17:28:46 +0300

    KS-1-2 посчитала работников

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksText: String = "Thank you for your order!"
+
+    println("Number of orders is $numberOfOrders.")
+    println(thanksText)
+
+    var amountOfWorkers: Int = 2000
+    // println("Amount of workers is $amountOfWorkers.")
+
+    amountOfWorkers = amountOfWorkers-1
+
+    println("New amount of workers is $amountOfWorkers,")
+}

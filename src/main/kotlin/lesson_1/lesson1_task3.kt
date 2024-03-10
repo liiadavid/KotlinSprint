dbeac4e4232b23e9501bc620e7f753b99cca7d96
liiadavid <liiadavid@yandex.ru>
2024-03-10 16:36:33 +0300

    KS-1-3 int to string

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,16 +2,14 @@ package lesson_1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
     println(year)
-    // println(hour.toString().padStart(2, '0'))
-    // println(minute.toString().padStart(2, '0'))
-    println("0$hour")
-    println("0$minute")
+    println(hour)
+    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print("$hour:$minute")
 }

fe140b0d87fcb361ea5903f7ecc0df5983f0b45a
liiadavid <liiadavid@yandex.ru>
2024-03-10 17:39:09 +0300

    KS-1-5 use String.format()

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -6,6 +6,7 @@ fun main() {
     val hour = (seconds / 3600) % 24
     val minute = (seconds / 60) % 60
     val second = seconds % 60
-    val timeInSpace = "0$hour:$minute:0$second"
-    println(timeInSpace)
+    val timeInSpace = String.format("%02d:%02d:%02d", hour, minute, second)
+
+    println("Время, проведенное в космосе - $timeInSpace")
 }

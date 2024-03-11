6c0b83faad7f2e41d4f7175cb39a31dd559cf653
liiadavid <liiadavid@yandex.ru>
2024-03-11 14:12:15 +0300

    KS-3-1 output greetings

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package lesson_3
+
+fun main() {
+    var greetings = "Добрый день"
+    val name = "Мария"
+    println("$greetings, $name!")
+
+    greetings = "Добрый вечер"
+    println("$greetings, $name!")
+}

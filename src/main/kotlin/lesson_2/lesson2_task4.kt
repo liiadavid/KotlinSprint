3285314e5be4755e34931a8e7733e3329b651395
liiadavid <liiadavid@yandex.ru>
2024-03-11 10:54:03 +0300

    KS-2-4 changes in calculations

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,8 @@ package lesson_2
 fun main() {
     val crystallineOre = 7
     val ironOre = 11
-    val crystallineOreWithBuff = crystallineOre + crystallineOre * 0.2
-    val ironOreWithBuff = ironOre + ironOre * 0.2
+    val percent = 20
 
-    println("Количество бонусной кристаллической руды = " + ((crystallineOreWithBuff - crystallineOre).toInt()))
-    println("Количество бонусной железной руды = " + ((ironOreWithBuff - ironOre).toInt()))
+    println("Количество бонусной кристаллической руды = " + (((crystallineOre.toDouble() / 100) * percent).toInt()))
+    println("Количество бонусной железной руды = " + ((ironOre.toDouble() / 100) * percent).toInt())
 }

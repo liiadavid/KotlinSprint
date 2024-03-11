44f4a73a775a95c287d4d93956207b0519c397b6
liiadavid <liiadavid@yandex.ru>
2024-03-11 17:58:49 +0300

    KS-3-2 basic create

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    val lastName = "Андреева"
+    val marriageLastName = "Сидорова"
+    val firstName = "Татьяна"
+    val patronymicName = "Сергеевна"
+    val age = 20
+    val marriageAge = 22
+
+    println("$lastName $firstName $patronymicName, $age лет")
+    println("$marriageLastName $firstName $patronymicName, $marriageAge года")
+}

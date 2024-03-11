bf977743aaa476e5b9feb5ad3bc7addfd896c52b
liiadavid <liiadavid@yandex.ru>
2024-03-11 14:32:05 +0300

    KS-1-5 add const

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,11 +1,16 @@
 package lesson_1
 
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
+const val MINUTES_IN_HOUR = 60
+const val HOURS_IN_DAY = 24
+
 fun main() {
     val seconds = 6480
 
-    val hour = (seconds / 3600) % 24
-    val minute = (seconds / 60) % 60
-    val second = seconds % 60
+    val hour = (seconds / SECONDS_IN_HOUR) % HOURS_IN_DAY
+    val minute = (seconds / SECONDS_IN_MINUTE) % MINUTES_IN_HOUR
+    val second = seconds % SECONDS_IN_MINUTE
     val timeInSpace = String.format("%02d:%02d:%02d", hour, minute, second)
 
     println("Время, проведенное в космосе - $timeInSpace")

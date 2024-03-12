81ae189fe4e9b929866b98db2e6f2ecee07cc950
liiadavid <liiadavid@yandex.ru>
2024-03-12 18:30:51 +0300

    KS-4-1 add const

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,9 +1,13 @@
 package lesson_4
 
 fun main() {
-    val numberOfTables = 13
     val todayReservedTables = 13
     val tomorrowReservedTables = 9
 
-    println("[Доступность столиков на сегодня: ${todayReservedTables < numberOfTables}],\n[Доступность столиков на завтра: ${tomorrowReservedTables < numberOfTables}].")
+    println(
+        "[Доступность столиков на сегодня: ${todayReservedTables < NUMBER_OF_TABLES}],\n" +
+                "[Доступность столиков на завтра: ${tomorrowReservedTables < NUMBER_OF_TABLES}]."
+    )
 }
+
+const val NUMBER_OF_TABLES = 13

1d7a40d69334e1898faadd73bfe9615220d1b45f
liiadavid <liiadavid@yandex.ru>
2024-03-12 18:36:32 +0300

    KS-4-2 add const

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,16 +1,31 @@
 package lesson_4
 
 fun main() {
-    val averageMinWeight = 35
-    val averageMaxWeight = 100
-    val averageMaxVolume = 100
     var cargoWeight = 20
     var cargoVolume = 80
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${(averageMinWeight < cargoWeight) && (cargoWeight <= averageMaxWeight) && (cargoVolume < averageMaxVolume)}")
+    println(
+        "Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
+                "${
+                    (AVERAGE_MIN_WEIGHT < cargoWeight) &&
+                            (cargoWeight <= AVERAGE_MAX_WEIGHT) &&
+                            (cargoVolume < AVERAGE_MAX_VOLUME)
+                }"
+    )
 
     cargoWeight = 50
     cargoVolume = 100
 
-    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${(averageMinWeight < cargoWeight) && (cargoWeight <= averageMaxWeight) && (cargoVolume < averageMaxVolume)}.")
+    println(
+        "Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': " +
+                "${
+                    (AVERAGE_MIN_WEIGHT < cargoWeight) &&
+                            (cargoWeight <= AVERAGE_MAX_WEIGHT) &&
+                            (cargoVolume < AVERAGE_MAX_VOLUME)
+                }."
+    )
 }
+
+const val AVERAGE_MIN_WEIGHT = 35
+const val AVERAGE_MAX_WEIGHT = 100
+const val AVERAGE_MAX_VOLUME = 100

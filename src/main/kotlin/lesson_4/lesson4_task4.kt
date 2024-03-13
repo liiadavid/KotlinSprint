81fe08dc79f5b759d2d82c88939d13ec7b622303
liiadavid <liiadavid@yandex.ru>
2024-03-13 21:15:13 +0300

    KS-4-4 add isEven

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,11 +2,12 @@ package lesson_4
 
 fun main() {
     val numberOfDay = 5
+    val isEven: Boolean = numberOfDay % 2 == 0
 
     println(
-        "Упражнения для рук: ${(numberOfDay % 2 == 1)}\n" +
-                "Упражнения для ног: ${(numberOfDay % 2 == 0)}\n" +
-                "Упражнения для спины: ${(numberOfDay % 2 == 0)}\n" +
-                "Упражнения для пресса: ${(numberOfDay % 2 == 1)}"
+        "Упражнения для рук: ${!isEven}\n" +
+                "Упражнения для ног: $isEven\n" +
+                "Упражнения для спины: $isEven\n" +
+                "Упражнения для пресса: ${!isEven}"
     )
 }

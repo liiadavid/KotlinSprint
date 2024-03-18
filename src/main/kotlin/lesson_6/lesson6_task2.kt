efe76497e39a2948de07abe3c7649aa7bc979ccd
liiadavid <liiadavid@yandex.ru>
2024-03-18 11:35:37 +0300

    KS-6-2 no cycle

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -2,11 +2,8 @@ package lesson_6
 
 fun main() {
     print("Сколько секунд засечь? - ")
-    val time = readln().toInt()
-    var timer = time
-    while (timer > 0) {
-        timer--
-        Thread.sleep(1000)
-    }
+    val time = readln().toLong()
+    Thread.sleep(time*1000)
+
     println("Прошло $time секунд.")
 }

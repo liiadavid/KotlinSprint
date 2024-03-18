7fdf5ddb0ae94c2f5668cfc49ce781453f385713
liiadavid <liiadavid@yandex.ru>
2024-03-18 21:42:30 +0300

    KS-6-2 add spaces

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -3,7 +3,7 @@ package lesson_6
 fun main() {
     print("Сколько секунд засечь? - ")
     val time = readln().toLong()
-    Thread.sleep(time*1000)
+    Thread.sleep(time * 1000)
 
     println("Прошло $time секунд.")
 }

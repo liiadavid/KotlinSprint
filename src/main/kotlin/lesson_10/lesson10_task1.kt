e1b225bbc3319152e86a162439aa9ab110e4c89d
liiadavid <liiadavid@yandex.ru>
2024-03-22 19:21:17 +0300

    KS-10-1 add when

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -3,21 +3,17 @@ package lesson_10
 import kotlin.random.Random
 
 fun main() {
-    var player = 0
-    var computer = 0
-
-    player += rollADie()
+    val player = rollADie()
     println("Ход человека.\nВыпавшее значение -  $player\n")
 
-    computer += rollADie()
+    val computer = rollADie()
     println("Ход компьютера.\nВыпавшее значение - $computer\n")
 
-    if (computer > player)
-        println("Победила машина.")
-    else if (computer < player)
-        println("Победило человечество.")
-    else
-        println("Победила дружба.")
+    when {
+        player < computer -> println("Победила машина.")
+        player > computer -> println("Победило человечество.")
+        player == computer -> println("Победила дружба.")
+    }
 }
 
 fun rollADie(): Int = Random.nextInt(1, 7)

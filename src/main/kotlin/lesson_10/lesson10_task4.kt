8c11fc4abf7031b0d530901af8dab54ebcd879a3
liiadavid <liiadavid@yandex.ru>
2024-03-23 19:22:23 +0300

    KS-10-4 remove variables

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -3,18 +3,18 @@ package lesson_10
 import kotlin.random.Random
 
 fun main() {
-    var man = 0
-    var computer = 0
+    var man: Int
+    var computer: Int
     var wonGames = 0
 
     do {
-        man = rollADie(man)
+        man = rollTheDice()
         println("Ход человека.\nВыпавшее значение - $man\n")
 
-        computer = rollADie(computer)
+        computer = rollTheDice()
         println("Ход компьютера.\nВыпавшее значение - $computer\n")
 
-        getTheWin(man, computer)
+        println(getTheWin(man, computer))
 
         wonGames += countWins(man, computer)
         println()
@@ -24,32 +24,22 @@ fun main() {
     println("Конец игры. Выигранных партий: $wonGames.")
 }
 
+fun rollTheDice(): Int = Random.nextInt(1, 7)
+
 fun continueTheGame(): Boolean {
     println("Хотите бросить кости еще раз? Введите Да или Нет")
-    val answer = readln()
-    println()
-
-    return answer.equals("да", true)
+    return readln().equals("да", true)
 }
 
-fun rollADie(player: Int): Int {
-    val number = Random.nextInt(1, 7)
-    val player = number
-
-    return player
-}
-
-fun getTheWin(man: Int, computer: Int) {
-    when {
-        man > computer -> println("Победило человечество.")
-        man < computer -> println("Победила машина.")
-        man == computer -> println("Победила дружба.")
+fun getTheWin(man: Int, computer: Int): String {
+    return when {
+        man > computer -> "Победило человечество."
+        man == computer -> "Победила дружба."
+        else -> "Победила машина."
     }
 }
 
 fun countWins(man: Int, computer: Int): Int {
-    var wins = 0
-    if (man > computer) wins += 1
-
-    return wins
+    return if (man > computer) 1
+    else 0
 }
